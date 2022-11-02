314ba423c40b3c77b794c7ee4d5b2bc08ebcb3ac
redyouzi <1589029587@qq.com>
2022-11-02 22:47:07 +0800

    代理模式

--- designPattern/Proxy/src/com/test/main/Test.java
+++ designPattern/Proxy/src/com/test/main/Test.java
@@ -7,8 +7,11 @@ public class Test {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
         UserService proxy = UserServiceProxy.getProxy(userService);
+
         proxy.login();
+
         proxy.delete();
+
         proxy.select();
     }
 }
